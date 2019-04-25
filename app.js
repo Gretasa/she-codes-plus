835ab4c96d086d0c8bcf64a7b1aa0b134d896b8d
Greta <greta@saffer.de>
2019-04-25 13:11:25 +0200

    adding all the functions
    But now i changed the function search (city) let apiUrl = `${apiRoot}/weather?q=Lisbon &appid=${apiKey}`;
    axios.get(apiUrl).then(updateWeather); It worked with this one

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 let place = document.querySelector("#weather-city");
 let messuredTime = document.querySelector("#weather-messured-time");
+let description = document.querySelector("#weather-description");
+let temperature = document.querySelector("#weather-temperature");
+let humidity = document.querySelector("#weather-humidity-level");
+let windSpeed = document.querySelector("#weather-wind-speed");
+let icon = document.querySelector("#weather-icon");
 
 let apiKey = "ce59557402d1a141a0672c568545cc55";
 let apiRoot = "https://api.openweathermap.org/data/2.5";
+let defaultCity = "Lisbon";
 //let apiPath = "weather";
+
+function formatDate(date) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDate()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours} : ${minutes}`;
+}
 
 function updateWeather(response) {
   place.innerHTML = response.data.name;
-  messuredTime.innerHTML = response.data.dt;
+  messuredTime.innerHTML = formatDate(new Date(response.data.dt * 1000));
+  description.innerHTML = response.data.weather[0].description;
+  let iconUrl = `http://openweathermap.org/img/w/${
+    response.data.weather[0].icon
+  }.png`;
+  icon.setAttribute("src", iconUrl);
+  icon.setAttribute("alt", response.data.weather[0].description);
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  humidity.innerHTML = response.data.main.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
 }
-let apiUrl = `${apiRoot}/weather?q=Lisbon &appid=${apiKey}`;
-axios.get(apiUrl).then(updateWeather);
+
+function search(city) {
+  let apiUrl = `${apiRoot}/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 
 // let place= document.querySelector("#weather-city");
 // let timestamp= docuemtn.querySelector ("#weather-timestamp");
